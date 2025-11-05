5cac8b48f8088be7265f248c2f248accee65f887
Harikeshav R <r.harikeshav@gmail.com>
2025-11-05 11:42:34 -0500

    feat: add command to install rosetta

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # Install XCode
 sudo xcode-select --install
 
+# Install rosetta
+softwareupdate --install-rosetta
+
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -13,4 +16,4 @@ brew update
 brew install cmake fd gh git git-lfs lazygit neovim ninja node ripgrep uv zsh
 
 # Install brew casks
-brew install --cask docker-desktop dotnet-sdk font-fira-code-nerd-font font-jetbrains-mono-nerd-font iterm2 meta-quest-developer-hub microsoft-office-businesspro transmission unity-hub vlc whatsapp zoom 
+brew install --cask docker-desktop dotnet-sdk font-fira-code-nerd-font font-jetbrains-mono-nerd-font iterm2 meta-quest-developer-hub microsoft-office-businesspro spotify transmission unity-hub vlc whatsapp zoom 
